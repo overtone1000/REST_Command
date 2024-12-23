1908e005d01a0facdd492aaf98eabd32c538c934
Tyler Moore <TylerRMoore@gmail.com>
2024-12-22 21:13:18 -0800

    Nix build compatibility

--- package/build.nix
+++ package/build.nix
@@ -1,9 +1,12 @@
 { pkgs ? import <nixpkgs> { } }:
+#Ensure nixpkgs is up to date. Check the channel currently used with sudo nix-channel --list (it's the one named nixos) and the rustc version with rustc -V
+
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "rest_commands";
   version = "0.1";
   src = pkgs.lib.cleanSource ../core/.;
   cargoLock={
     lockFile = ../core/Cargo.lock;
+    allowBuiltinFetchGit = true;
   };
 }

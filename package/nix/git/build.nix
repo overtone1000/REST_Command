8d2de76654f2c5ca1359ec9cb1d6a0daac9ebded
Tyler Moore <TylerRMoore@gmail.com>
2024-12-24 16:08:26 -0800

    Service may be workable now

--- package/nix/git/build.nix
+++ package/nix/git/build.nix
@@ -3,22 +3,35 @@
 let 
   repo = fetchGit {
     url = "https://github.com/overtone1000/REST_Commands.git";
-    rev = "c7fb4e8bbfd4bc84be4ffcccc94bdafa7ff8882a";
+    rev = "a80abf4617c447f3bfa46100760cfb98114a8994";
   };
 
   manifest = (pkgs.lib.importTOML ("${repo}/core/Cargo.toml")).package;
   lock = ("${repo}/core/Cargo.lock");
-in
 
-pkgs.rustPlatform.buildRustPackage rec {
-  pname = manifest.name;
-  version = manifest.version;
+  package = pkgs.rustPlatform.buildRustPackage {
+    pname = manifest.name;
+    version = manifest.version;
+    
+    src = "${repo}/core";
+
+    #cargoHash = ""; #Determine correct checksum by attempting build and viewing error output
+    cargoLock={
+      lockFile = (lock);
+      allowBuiltinFetchGit = true;
+    };
+  };
   
-  src = "${repo}/core";
-
-  #cargoHash = ""; #Determine correct checksum by attempting build and viewing error output
-  cargoLock={
-    lockFile = (lock);
-    allowBuiltinFetchGit = true;
+  port = 30123;
+  dir = "/var";
+in
+{
+  systemd.services.rest_command = {
+    wantedBy = ["multi-user.target"];
+    after = ["network.target"];
+    script = "${package}/bin/manifest.name ${port} ${dir}";
+    serviceConfig = {
+      Restart = "always";
+    };
   };
 }

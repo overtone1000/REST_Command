d3b769a3ed9d5d30015e43e991ca4c2021c69867
Tyler Moore <TylerRMoore@gmail.com>
2025-01-03 17:09:35 -0800

    Try shallow builds

--- package/nix/git/build.nix
+++ package/nix/git/build.nix
@@ -5,8 +5,8 @@ let
   repo = fetchGit {
     url = "https://github.com/overtone1000/REST_Commands.git";
     ref = "main"; #this does seem to be necessary
-    #shallow = true;
-    rev = "4ebf990e1bedd27464f033f5dfd046a1ec610e43"; #sometimes need to force it to rebuild
+    shallow = true;
+    #rev = "4ebf990e1bedd27464f033f5dfd046a1ec610e43"; #sometimes need to force it to rebuild
   };
 
   manifest = (pkgs.lib.importTOML ("${repo}/core/Cargo.toml")).package;
